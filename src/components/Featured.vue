8ffcfe4e8b5b7239a664eb4283362905942285ae
Avi <aviblinder@gmail.com>
2022-01-09 11:20:45 +0200

    Featured products fixed

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -1,63 +1,58 @@
 <template>
   <div class="bg-white">
-    <div class="inset-0 mx-auto px-4 py-6 sm:px-6 lg:px-8">
-      <div class="bg-gray-50 rounded-2xl px-6 py-16 sm:p-16">
-        <div class="max-w-xl mx-auto lg:max-w-none">
-          <div class="text-center">
-          <h2 id="products-heading" class="sr-only">Products</h2>
-          </div>
-          <div v-if="loaded">
-            <div class="flex justify-center items-center">
-              <div
-                class="text-2xl text-centered bg-secondary-200 rounded-full 
-                px-4 py-4
-                mb-6 md:px-60"
-              >
-                Featured Products
-              </div>
-            </div>
-            <div
-              class="grid sm-grid-cols-2 md:grid-cols-9 gap-x-3 gap-y-8 sm:ml-6 "
-            >
-              <!-- Product grid -->
-              <div
-                class="relative grid gap-y-10 gap-x-2 
-                md:col-span-3 md:gap-x-2 sm:grid-2 sm:border-b-2 
-                max-w-4xl w-3/4 sm:ml-6 md:ml-12 pb-3"
-                v-for="product in products"
-                :key="product.uuid"
-              >
-                <div
-                  v-if="product.content.valueProposition"
-                  class="absolute flex w-9 h-9 top-2 right-2 bg-green-300 z-1 rounded-full p-1 text-center text-xs justify-center items-center"
-                >
-                  {{ product.content.valueProposition }}
-                </div>
+    <div
+      class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
+    >
+      <div v-if="loaded">
+        <h2 class="text-2xl font-semibold tracking-tight 
+         text-secondary-600">
+          Featured Products
+        </h2>
 
-                <router-link
-                  :to="{ name: 'product', params: { product: product.slug } }"
-                  class="group text-sm"
-                >
-                  <div
-                    class="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden group-hover:opacity-80 hover:border-gray-400 border-2 shadow-lg bg-white"
+        <div
+          class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 
+          lg:grid-cols-4 xl:gap-x-8"
+        >
+          <div
+            v-for="product in products"
+            :key="product.uuid"
+            class="group relative shadow-md p-2 border-gray-200"
+
+          >
+            <!--  -->
+            <div
+              v-if="product.content.valueProposition"
+              class="absolute flex w-9 h-9 top-2 right-2 bg-green-300 z-1 rounded-full p-1 text-center text-xs justify-center items-center"
+            >
+              {{ product.content.valueProposition }}
+            </div>
+            <!--  -->
+            <div
+              class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none"
+            >
+              <img
+                :src="product.content.images[0].filename"
+                :alt="product.content.images[0].alt"
+                class="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div class="mt-4 flex justify-between">
+              <div>
+                <h3 class="text-sm text-secondary-700">
+                  <router-link
+                    :to="{ name: 'product', params: { product: product.slug } }"
                   >
-                    <img
-                      :src="product.content.images[0].filename"
-                      :alt="product.content.images[0].alt"
-                      class="w-full h-full object-center object-cover"
-                    />
-                  </div>
-                  <div class="bg-green-100 mt-2 py-2">
-                    <h3 class="mt-4 font-medium text-gray-900">
-                      {{ product.name }}
-                    </h3>
-                    <p class="mt-2 font-medium text-gray-900">
-                      {{ product.content.price }} {{ product.content.currency }}
-                    </p>
-                    <p>Size: {{ product.content.size }}</p>
-                  </div>
-                </router-link>
+                    <span aria-hidden="true" class="absolute inset-0" />
+                    {{ product.name }}
+                  </router-link>
+                </h3>
+                <p class="mt-1 text-sm text-gray-500">
+                  Size: {{ product.content.size }} oz
+                </p>
               </div>
+              <p class="text-sm font-medium text-gray-900">
+                {{currencySign}}{{ product.content.price }} 
+              </p>
             </div>
           </div>
         </div>
@@ -67,12 +62,14 @@
 </template>
 
 <script>
-  // import { ref } from 'vue';
   import { ref, inject } from 'vue';
+  import { useStore } from 'vuex';
 
   export default {
     setup() {
+      const store = useStore();
       const storyapi = inject('storyapi');
+      const currencySign = store.getters['general/getCurrencySign'];
 
       const getProducts = async (version) => {
         try {
@@ -99,6 +96,7 @@
       return {
         products,
         loaded,
+        currencySign
       };
     },
   };
