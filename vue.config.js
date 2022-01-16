5f862e1cf624ef60142a7bf75ae5d1921de40445
Avi <aviblinder@gmail.com>
2022-01-16 12:07:11 +0200

    title fix

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const vueSrc = './src';
 console.log('webpack ', path.join(__dirname, vueSrc));
 module.exports = {
   configureWebpack: {
-    title: 'Coolaquas Store',
     devtool: 'source-map',
     resolve: {
       alias: {
